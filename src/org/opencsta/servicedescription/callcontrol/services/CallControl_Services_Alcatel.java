6dcd9343a424708fa1dbf5faa95897e139fea20e
Chris Mylonas <chris@opencsta.org>
2010-05-31 13:07:20 +0000

    Added Alcatel Call Control Services class skeleton.

---
+++ src/org/opencsta/servicedescription/callcontrol/services/CallControl_Services_Alcatel.java
@@ -0,0 +1,52 @@
+package org.opencsta.servicedescription.callcontrol.services;
+
+public class CallControl_Services_Alcatel implements CallControl_Services {
+
+	public StringBuffer AnswerCall(String dev, String strCallId_0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer ClearConnection(String dev, String strCallId_0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer DeflectCall(String deviceFrom, String deviceTo,
+			String strCallId_0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer HoldCall(String dev, String strCallId_0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer MakeCall(String dev1, String dev2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer MonitorStart(String device) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer MonitorStop(String crossRef) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer RetrieveCall(String dev, String strCallId_0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StringBuffer SingleStepTransfer(String deviceFrom, String deviceTo,
+			String strCallId_0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
